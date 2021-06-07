d296116decc7f0e44dfa1f4d63954c525f5e89f1
Iris Perisa <iris.perisa@gmail.com>
2021-06-07 17:46:56 +0200

    fixed model Chef

--- db/migrate/20210607154326_create_chefs.rb
+++ db/migrate/20210607154326_create_chefs.rb
@@ -5,6 +5,7 @@ class CreateChefs < ActiveRecord::Migration[6.0]
       t.string :location
       t.text :description
       t.float :price
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_152759) do
+ActiveRecord::Schema.define(version: 2021_06_07_154326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 2021_06_07_152759) do
     t.string "location"
     t.text "description"
     t.float "price"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_chefs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_07_152759) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chefs", "users"
 end
