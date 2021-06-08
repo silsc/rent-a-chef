31d6b569b0bba0476c8ee8efa32a23e9b104b43f
Carlos Villen <carlosvillen@gmail.com>
2021-06-08 11:26:21 +0100

    Added create actions in chefs controller.

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,20 @@
 class ChefsController < ApplicationController
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      redirect_to @chef
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :price, :location, :description)
+  end
 end
