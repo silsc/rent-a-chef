745a63cfca55f1107325aaa494de8a88ea397501
Iris Perisa <iris.perisa@gmail.com>
2021-06-09 12:44:26 +0200

    added avatar attach option to chef model, chefs controller and create new chef form

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,7 @@ class Chef < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_one_attached :photo
+  has_one_attached :avatar
 
   validates :name, :price, presence: true
 end
