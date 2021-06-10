9b7f997e24fc351e7fc077ba2973e9b0340b7a9e
Silvia Salat <salat.silvia@gmail.com>
2021-06-10 15:19:29 +0200

    added the featured view and the search function

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,4 +9,11 @@ class Chef < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location,
+    against: [:location],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
