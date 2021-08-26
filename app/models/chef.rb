6ad74f26db18f66b21613bf7d184e51853f213d1
Silvia Salat <salat.silvia@gmail.com>
2021-08-26 04:09:24 +0200

    RESET commit version

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,15 +9,15 @@ class Chef < ApplicationRecord
   has_one_attached :avatar
 
   validates :name, :price, presence: true
-  validates :category, inclusion: {in: CATEGORIES}
+  validates :category, inclusion: { in: CATEGORIES }
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
   include PgSearch::Model
   pg_search_scope :search_by_location,
-    against: [:location],
-    using: {
-      tsearch: { prefix: true }
-    }
+                  against: :location,
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
